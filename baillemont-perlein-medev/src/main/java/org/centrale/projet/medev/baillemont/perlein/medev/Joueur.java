0a1b41f067719117f406b878cc269b191b1d7292
hperlein <hector.perlein@hotmail.fr>
2025-01-13 08:42:59 +0100

    développement classe joueur

--- baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/Joueur.java
+++ baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/Joueur.java
@@ -10,4 +10,29 @@ package org.centrale.projet.medev.baillemont.perlein.medev;
  */
 public class Joueur {
     
+    private String nom;
+    private Pion couleur;
+
+    public Joueur(String nom, Pion couleur) {
+        this.nom = nom;
+        this.couleur = couleur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Pion getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Pion couleur) {
+        this.couleur = couleur;
+    }
+    
+    
 }
