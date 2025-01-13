7de6274b69a17789dc3bbdb9aa4a0167d9518465
momo838 <morgane0044@sfr.fr>
2025-01-13 08:26:33 +0100

    Création de la classe Pion

---
+++ baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/Pion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.projet.medev.baillemont.perlein.medev;
+
+/**
+ *
+ * @author morga
+ * Classe énumérative Pion. Le pion est noir, blanc, ou représente une case vide.
+ */
+public enum Pion {
+    NOIR, BLANC, VIDE
+}
