98b2d0254eb3d8052e97f10f1f7beb6b6480021c
hperlein <hector.perlein@hotmail.fr>
2025-01-13 09:16:49 +0100

    développement main - initialisation

--- baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/BaillemontPerleinMedev.java
+++ baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/BaillemontPerleinMedev.java
@@ -10,6 +10,19 @@ package org.centrale.projet.medev.baillemont.perlein.medev;
  */
 public class BaillemontPerleinMedev {
 
+    private Plateau plateau;
+    private Joueur joueur1;
+    private Joueur joueur2;
+    private Joueur joueur_actuel;
+
+    public BaillemontPerleinMedev() {
+        plateau = new Plateau();
+        joueur1 = new Joueur("Joueur 1", Pion.NOIR);
+        joueur2 = new Joueur("Joueur 2", Pion.BLANC);
+        joueur_actuel = joueur1;
+    }
+
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
