f56b44fc6a603f62f4bbe03f889f61039ba30772
hperlein <hector.perlein@hotmail.fr>
2025-01-13 09:53:37 +0100

    développement main : méthode jouer to do : développer méthodes dans plateau, afficher résultats

--- baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/BaillemontPerleinMedev.java
+++ baillemont-perlein-medev/src/main/java/org/centrale/projet/medev/baillemont/perlein/medev/BaillemontPerleinMedev.java
@@ -3,6 +3,7 @@
  */
 
 package org.centrale.projet.medev.baillemont.perlein.medev;
+import java.util.Scanner;
 
 /**
  *
@@ -22,6 +23,57 @@ public class BaillemontPerleinMedev {
         joueur_actuel = joueur1;
     }
 
+        public void jouer() {
+            
+        //to do : dans plateau, implémenter mouvementValide, jouerMouvement et peutJouer
+            
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            plateau.afficher();
+            System.out.println(joueur_actuel.getNom() + "Entrer le mouvement (ex: e5) : ");
+            String mouvement = scanner.nextLine();
+
+            // vérif validité du mouvement
+            if (mouvement.length() < 2) {
+                System.out.println("Non valide");
+                continue;
+            }
+
+            // convertir en coordonnées
+            int colonne = mouvement.charAt(0) - 'a';
+            int ligne = Character.getNumericValue(mouvement.charAt(1)) - 1;
+
+            // vérif validité des coordonnées
+            if (ligne < 0 || ligne >= 8 || colonne < 0 || colonne >= 8) {
+                System.out.println("Non valide");
+                continue;
+            }
+
+            //test si le mouvement est ok
+            
+            if (plateau.mouvementValide(ligne, colonne, joueur_actuel.getCouleur())) {
+                
+                //jouer le tour
+                plateau.jouerMouvement(ligne, colonne, joueur_actuel.getCouleur());
+                
+                // changement de joueur
+                joueur_actuel = (joueur_actuel == joueur1) ? joueur2 : joueur1;
+            } 
+            
+            else {
+                System.out.println("Non valide");
+            }
+
+            // vérif si partie finie ou non
+            if (!plateau.peutJouer(joueur1.getCouleur()) && !plateau.peutJouer(joueur2.getCouleur())) {
+                System.out.println("Partie terminée. QUi a gagné ?!?!");
+                break;
+            }
+        }
+        scanner.close();
+
+        //afficher résultat
+}
     
     public static void main(String[] args) {
         System.out.println("Hello World!");
